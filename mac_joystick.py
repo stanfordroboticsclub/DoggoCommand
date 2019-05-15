fe0294b7adf1e900d31eb52648b49ec48192ac55
Michal Adamkiewicz <mikh3x4@gmail.com>
2019-05-15 16:14:28 -0700

    added joystick reading

---
+++ mac_joystick.py
@@ -0,0 +1,72 @@
+import os
+import pygame
+from UDPComms import Publisher
+import serial
+import math
+
+os.environ["SDL_VIDEODRIVER"] = "dummy"
+
+pygame.display.init()
+pygame.joystick.init()
+
+# wait untill joystick is connected
+while 1:
+    try:
+        pygame.joystick.Joystick(0).init()
+        break
+    except pygame.error:
+        pygame.time.wait(500)
+
+# Prints the joystick's name
+JoyName = pygame.joystick.Joystick(0).get_name()
+print("Name of the joystick:")
+print(JoyName)
+# Gets the number of axes
+JoyAx = pygame.joystick.Joystick(0).get_numaxes()
+print("Number of axis:")
+print(JoyAx)
+
+# doggo = serial.Serial('/dev/tty.usbserial-DN042CH3', 115200)
+
+def deadband(x, deadband):
+    if math.fabs(x) < deadband:
+        return 0
+    else:
+        return x
+
+while True:
+    pygame.event.pump()
+
+    forward = -pygame.joystick.Joystick(0).get_axis(1)
+    right   = pygame.joystick.Joystick(0).get_axis(0)
+    
+    forward = deadband(forward, 0.1)
+    right   = deadband(right, 0.1)
+
+    b_button    = pygame.joystick.Joystick(0).get_button(2)
+    a_button    = pygame.joystick.Joystick(0).get_button(1)
+
+    x_button    = pygame.joystick.Joystick(0).get_button(0)
+    y_button    = pygame.joystick.Joystick(0).get_button(3)
+
+    # code to figure out what axis is which
+    # for i in range(4):
+    #     print(i, pygame.joystick.Joystick(0).get_axis(i))
+    # for i in range(12):
+    #     print(i, pygame.joystick.Joystick(0).get_button(i))
+
+    print("fwd", forward, "turn", right, 
+                "x",x_button, "y",y_button, 
+                "a",a_button, "b",b_button)
+
+    if forward == 0 and right == 0:
+        pass
+        # doggo.write("S\n")
+    elif right == 0:
+        pass
+    elif forward == 0:
+        pass
+    else:
+        pass
+
+    pygame.time.wait(100)
