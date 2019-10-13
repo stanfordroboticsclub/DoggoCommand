62ade48b6ff18fcb67df44b9a8214fb80061a044
Maximus Reisner <maximusreisner@DN0a1f2c71.SUNet>
2019-10-12 18:36:06 -0700

    added gitignore to ignore .csv's
    realtimegrapher has it's first working verson
    Untested on real robot, tested on arduino publishing CSV data

--- realtimeDataGrapher.py
+++ realtimeDataGrapher.py
@@ -1,47 +1,76 @@
 #PYTHON 3
+#Assumes the micro is printing a comma seperated list with a space
+#e.i.
+#2.3, 4.234, 5432445.34523, etc
+
+#CHANGE SERIAL PORT BAUD RATE AND PORT PATH
+#If problems parsing data, check the .split on line 21
 
 import numpy as np
 import math
 import random
 import csv
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import time
 import serial
+import datetime as dt
 
-def findNextBlank(line, index):
-	for i in range(index, len(line)):
-		if line[i] == "\t":
-			return i
-	return -1
+NUM_DATAPOINTS = 20
+
+def animate(i, ser, data, timeArray, commas, ax, outputChannels):
+	line = ser.readline().decode("utf-8").strip()
+	currentLine = line.split(", ")
+	for i in range (0, commas):
+		data[i].append(currentLine[i])
+
+	timeArray.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+	ax.clear()
+
+	for i in range(0, commas):
+		ax.plot(timeArray[-NUM_DATAPOINTS:], data[i][-NUM_DATAPOINTS:])
+
+
+	plt.xticks(rotation=45, ha='right')
+	plt.subplots_adjust(bottom=0.30)
+	plt.title('Data over Time')
+	plt.ylabel('Data')
+	plt.xlabel('Time')
+
+
+
 
 def main():
 	data = []
-	with serial.Serial('/dev/ttys001', 115200, timeout=1) as ser:
+	commas = 1
+
+	fig = plt.figure()
+	ax = fig.add_subplot(1, 1, 1)
+	timeArray = []
+
+	outputChannels = input("Type a Tuple for which indexs to graph: ")
+	csvFileName = input("CSV output file name (ending in .csv): ")
+
+
+	#nano is 9600, teensy is 115200
+	with serial.Serial('/dev/cu.usbserial-AE01ISIZ', 9600, timeout=1) as ser:
+		time.sleep(.5)
+
 		line = ser.readline().decode("utf-8").strip()
-		tabs = 1
-		for char in line[:len(line)-1]:
-			if (char == "\t"):
-				tabs += 1
+		for char in line:
+			if char == ",":
+				commas += 1
 				data.append([])
-		print(tabs)
-		print(line)
-		print(data)
-		time.sleep(.001)
-		while(True):
-			line = ser.readline().decode("utf-8").strip()
-			lastblank = 0
-			currentblank = findNextBlank(line, 0)
-			data[0].append(line[lastblank:currentblank])
-			lastblank = currentblank
-			currentblank = findNextBlank(line, lastblank+1)
+		#last entry doesn't have a comma
+		data.append([])
 
-			for i in range (1, tabs-1):
-				data[i].append(line[lastblank+1:currentblank])
-				lastblank = currentblank
-				currentblank = findNextBlank(line, lastblank+1)
+		ani = animation.FuncAnimation(fig, animate, fargs=(ser, data, timeArray, commas, ax, outputChannels), interval=1)
+		plt.show()
 
-			print(data[3])
-			time.sleep(.001)
+	with open(csvFileName, 'w') as csvFile:
+		writer = csv.writer(csvFile)
+		writer.writerows(data)
+	csvFile.close()
 
 
 if __name__ == "__main__":
