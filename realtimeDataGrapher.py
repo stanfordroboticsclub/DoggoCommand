efe967dc41ab1ab16b19145ebb57002d14f04da2
Maximus Reisner <maximusreisner@DN0a1f2c71.SUNet>
2019-10-21 23:20:30 -0700

    Fixed to use multithreading for a less laggy expierence. Will save the data as a csv as defined in the constants section

--- realtimeDataGrapher.py
+++ realtimeDataGrapher.py
@@ -1,10 +1,10 @@
 #PYTHON 3
-#Assumes the micro is printing a comma seperated list with a space
-#e.i.
-#2.3, 4.234, 5432445.34523, etc
+#Change consts if not working
+#Assumes data is comma seperated with no ws besides \n e.i.:
+#1,2,3,41234,12351,1378534,452,4626
+#Only issue is that each graphing period will not save that data to the output,
+#This isn't too bad since there are so many datapoints tho
 
-#CHANGE SERIAL PORT BAUD RATE AND PORT PATH
-#If problems parsing data, check the .split on line 21
 
 import numpy as np
 import math
@@ -15,28 +15,58 @@ import matplotlib.animation as animation
 import time
 import serial
 import datetime as dt
+import threading
+import queue
 
 
-NUM_DATAPOINTS = 100
-outputChannels = (15,)
+#===================================
+#CONSTANTS
+#===================================
 
-def animate(i, ser, data, timeArray, ax, outputChannels):
-	line = ser.readline().decode("utf-8").strip()
-	currentLine = line.split("\t")
-	print(currentLine)
-	for i in range(0, len(data)):
-		data[i].append(float(currentLine[i]))
+
+#How many datapoints to display on the graph
+#20 is pretty realtime but larger values start to lag
+NUM_DATAPOINTS = 20
+
+#Which indexes of the input data to display
+OUTPUT_CHANNELS = (0,1,2)
+
+#nano is /dev/cu.usbserial-AE01ISIZ
+SERIAL_PORT = "/dev/cu.usbserial-AE01ISIZ"
+
+#nano is 9600, teensy is 115200
+BAUD_RATE = 9600
+
+OUTPUT_FILE_NAME = "test.csv"
+
+#===================================
+#FUNCTIONS
+#===================================
+
+
+def animate(i, dataQueue, plot, timeArray, dataArray):
+	currentData = dataQueue.get()
 
 	timeArray.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
-	ax.clear()
+	if len(timeArray) > NUM_DATAPOINTS:
+		del(timeArray[0])
 
-	for i in outputChannels:
-		ax.plot(timeArray[-NUM_DATAPOINTS:], data[i][-NUM_DATAPOINTS:])
+	dataArray.append(currentData)
+	if len(dataArray) > NUM_DATAPOINTS:
+		del(dataArray[0])
+
+	numpyArray = np.array(dataArray)
+	numpyArray = np.swapaxes(numpyArray, 0, 1)
+
+	plot.clear()
+
+	for i in OUTPUT_CHANNELS:
+		plot.plot(timeArray, numpyArray[i])
 
 	plt.tick_params(
 		axis='x',          # changes apply to the x-axis
 		which='both',      # both major and minor ticks are affected
-		bottom=False,      # ticks along the bottom edge are off
+		bottom=True,      # ticks along the bottom edge are off
  		top=False,         # ticks along the top edge are off
 		labelbottom=False)
 	plt.xticks(rotation=45, ha='right')
@@ -46,35 +76,56 @@ def animate(i, ser, data, timeArray, ax, outputChannels):
 	plt.xlabel('Time')
 
 
+def csvSaveThreadFunc(dataQueue):
+	with open(OUTPUT_FILE_NAME, 'w') as outputFile:
+		fileWriter = csv.writer(outputFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		while True:
+			fileWriter.writerow(dataQueue.get())
+			if dataQueue.qsize() > 10:
+				print("CAUTION: DataQueue above 10 Entries!")
+
+
+
+def serialThreadFunc(dataQueueArray):
+	print("SerialThread")
+	with serial.Serial(SERIAL_PORT, BAUD_RATE) as ser:
+		time.sleep(.5)
+		ser.readline()
+		while True:
+			currentLine = ser.readline().decode("utf-8").strip().split(",")
+			currentLine = [float(i) for i in currentLine]
+			dataQueueArray.put(currentLine)
+
+
+#===================================
+#MAIN
+#===================================
 
 
 def main():
-	data = []
+	dataQueueArray = queue.Queue()
+
+	timeArray = []
+	dataArray = []
 
 	fig = plt.figure()
-	ax = fig.add_subplot(1, 1, 1)
-	timeArray = []
+	subplot = fig.add_subplot(1, 1, 1)
 
-	csvFileName = input("CSV output file name (ending in .csv): ")
+	serialThread = threading.Thread(target=serialThreadFunc, args=(dataQueueArray,))
+	csvSaveThread = threading.Thread(target=csvSaveThreadFunc, args=(dataQueueArray,))
 
+	serialThread.daemon = True
+	serialThread.start()
 
-	#nano is 9600, teensy is 115200
-	with serial.Serial('/dev/cu.usbserial-DN042CH3', 115200, timeout=1) as ser:
-		time.sleep(.5)
+	csvSaveThread.daemon = True
+	csvSaveThread.start()
 
-		line = ser.readline().decode("utf-8").strip()
-		for element in line.split("\t"):
-			data.append([])
-		#last entry doesn't have a comma
-		ani = animation.FuncAnimation(fig, animate, fargs=(ser, data, timeArray, ax, outputChannels), interval=1)
-		plt.show()
-
-	with open(csvFileName, 'w') as csvFile:
-		writer = csv.writer(csvFile)
-		writer.writerows(data)
-	csvFile.close()
+	animated = animation.FuncAnimation(fig, animate, fargs=(dataQueueArray, subplot, timeArray, dataArray), interval=1)
+	plt.show()
 
 
 if __name__ == "__main__":
 	main()
+
+
 
